e9fd1ec36d8f280e3bb691d076988e0a02024830
Grace-Shao <gshao8@gatech.edu>
2024-11-02 17:49:34 -0400

    game state functions skeleton

--- M01_Scaffold/main.c
+++ M01_Scaffold/main.c
@@ -3,6 +3,86 @@
 #include "sprites.h"
 #include "print.h"
 
-int main() {
+void start();
+void goToStart();
+void instructions();
+void goToInstructions();
+void game();
+void goToGame();
+void goToPause();
+void pause();
+void goToWin();
+void win();
+void initialize();
+void goToLose();
+void lose();
+
+// States
+enum STATE {START, INSTRUCTIONS, GAME, PAUSE, LOSE, WIN} state;
+OBJ_ATTR shadowOAM[128];
+typedef enum {DOWN, RIGHT, UP, LEFT} DIRECTION;
+
+// Button Variables
+unsigned short buttons;
+unsigned short oldButtons;
+
+void initialize() {
+    // Set up basic registers 
+    REG_DISPCTL = MODE(0) | BG_ENABLE(2) | SPRITE_ENABLE;
+    REG_BG2CNT = BG_SCREENBLOCK(0) | BG_CHARBLOCK(2);
     
+    buttons = REG_BUTTONS;
+    oldButtons = 0;
+
+    mgba_open();
+    mgba_printf("initialized");
+    //initSound();
+    goToStart();
+}
+
+int main() {
+    initialize();
+    while(1) {
+        oldButtons = buttons;
+        buttons = REG_BUTTONS;
+             
+        // State Machine
+        switch(state) {
+            case START:
+                start();
+                break;
+            // case INSTRUCTIONS:
+            //     instructions();
+            //     break;
+            case GAME:
+                game();
+                break;
+            // case LOSE:
+            //     lose();
+            //     break;
+            default:
+                break;
+        }      
+    }
+}
+
+void goToStart() {
+    //initGame();
+}
+
+void start() {
+    state = START;
+    if (BUTTON_HELD(BUTTON_START)) {
+        mgba_printf("start button pressed");
+        goToGame();
+    }
+}
+
+void goToGame() {
+    mgba_printf("going to game");
+    state = GAME;
+}
+
+void game() {
+
 }
